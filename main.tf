5db32ac59814564f09b91543f23719b59cd00339
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-23 15:30:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = ">= 0.60.0"
+    }
+  }
+}
+
 provider "snowflake" {
-  account         = var.snowflake_account
-  username        = var.snowflake_username
-  role            = var.snowflake_role
-  password        = var.snowflake_password
+  account  = var.snowflake_account       # e.g., KIB31224
+  username = var.snowflake_username
+  password = var.snowflake_password
+  role     = var.snowflake_role
 }
 
 resource "snowflake_grant_ownership" "role_transfer" {
