99579e0a23dfdbe7e4b154261489cdd033b1122e
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-23 14:30:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ provider "snowflake" {
   account         = var.snowflake_account
   username        = var.snowflake_username
   role            = var.snowflake_role
-  private_key_path = var.snowflake_private_key_path
-  private_key_passphrase = var.snowflake_private_key_passphrase
-  region          = var.snowflake_region
+  passowrd        = var.snowflake_passowrd
 }
 
 resource "snowflake_grant_ownership" "role_transfer" {
   on {
     object_type = "ROLE"
-    object_name = "HCPSNOW"
+    object_name = "HCP_USER"
   }
 
   to {
