4c249b2ee7b0dcd01dc7fa95991c0d3cbeae360d
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-24 10:47:42 +0530

    Update grant_ownership.py

--- grant_ownership.py
+++ grant_ownership.py
@@ -1,31 +1,18 @@
-import json
 import os
-import sys
 import snowflake.connector
 
-# Required for Terraform external data source
-_ = json.load(sys.stdin)
+conn = snowflake.connector.connect(
+    user=os.environ["SNOWFLAKE_USER"],
+    password=os.environ["SNOWFLAKE_PASSWORD"],
+    account=os.environ["SNOWFLAKE_ACCOUNT"],
+    role=os.environ["SNOWFLAKE_ROLE"],
+    warehouse=os.environ["SNOWFLAKE_WAREHOUSE"]
+)
 
+cs = conn.cursor()
 try:
-    conn = snowflake.connector.connect(
-        user=os.environ["SNOWFLAKE_USER"],
-        password=os.environ["SNOWFLAKE_PASSWORD"],
-        account=os.environ["SNOWFLAKE_ACCOUNT"],
-        role=os.environ["SNOWFLAKE_ROLE"],
-        warehouse=os.environ["SNOWFLAKE_WAREHOUSE"]
-    )
-
-    cursor = conn.cursor()
-    cursor.execute("GRANT OWNERSHIP ON ROLE HCP_USER TO ROLE SYSADMIN COPY CURRENT GRANTS;")
-    print(json.dumps({"result": "success"}))
-
-except Exception as e:
-    print(json.dumps({"error": str(e)}))
-    sys.exit(1)
-
+    cs.execute("GRANT OWNERSHIP ON ROLE HCP_USER TO ROLE SYSADMIN COPY CURRENT GRANTS;")
+    print("Ownership transferred successfully.")
 finally:
-    try:
-        cursor.close()
-        conn.close()
-    except:
-        pass
+    cs.close()
+    conn.close()
